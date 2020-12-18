802596d529fe1f4cd93fb19f22edead512b79e3c
ogurenko <37590585+ogurenko@users.noreply.github.com>
2020-12-18 22:06:49 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/auth";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyChO48Osc1tI3KoFTGmrz0nIIpM9sd89oQ",
-  authDomain: "ecommerce-redux.firebaseapp.com",
-  databaseURL: "https://ecommerce-redux.firebaseio.com",
+  apiKey: "your API key",
+  authDomain: "your app domain",
+  databaseURL: "your database URL",
   projectId: "ecommerce-redux",
   storageBucket: "ecommerce-redux.appspot.com",
-  messagingSenderId: "539819473537",
-  appId: "1:539819473537:web:a5bef930dc908bd29b6b80",
+  messagingSenderId: "your messaging Id",
+  appId: "your appId",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
